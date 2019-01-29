d5aa72fa7987664fb0f2caff0673067409ddada3
Luke Barrett <gotluke@gmail.com>
2019-01-29 00:01:38 -0700

    update styling

---
+++ src/dropDown/DropDown.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './DropDown.css';
+
+export default class DropDown extends Component {
+    render(){
+        return(
+            <div>
+                <ul class='drop-down'>
+                    <li>1. Make this dropdown a React component.</li>
+                    <li>2. Demo the component in Framer X.</li>
+                    <li>3. Don't use any libraries.</li>
+                    <li>4. Final deliverable should be a Framer X file.</li>
+                    <li>Bonus: Add changable props in the Framer X interface.</li>
+                </ul>
+            </div>
+        )
+    }
+} 
